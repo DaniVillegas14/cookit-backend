f90b55f3b67ef36f579c4462de48a1c135b2240d
Dani Villegas <dani.villegascarp@gmail.com>
2020-09-01 13:08:14 -0300

    Config swagger

--- src/main/java/ar/edu/unq/cookitbackend/controller/ApiController.java
+++ src/main/java/ar/edu/unq/cookitbackend/controller/ApiController.java
@@ -2,6 +2,7 @@ package ar.edu.unq.cookitbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @GetMapping
-    public String test() {
+    public String test(@RequestParam("id") String id) {
         return "Probando 123...";
     }
 }
