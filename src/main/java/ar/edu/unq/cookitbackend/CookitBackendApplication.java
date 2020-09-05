f36ad4167fbf11e4d4e9bbb4421ad23d7d44712e
Uriel Quevedo <42658616+UrielQuevedo@users.noreply.github.com>
2020-09-05 09:59:18 -0300

    Configuro el cors (#6)

--- src/main/java/ar/edu/unq/cookitbackend/CookitBackendApplication.java
+++ src/main/java/ar/edu/unq/cookitbackend/CookitBackendApplication.java
@@ -3,17 +3,30 @@ package ar.edu.unq.cookitbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Configuration
 @EnableSwagger2
-public class CookitBackendApplication {
+public class CookitBackendApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CookitBackendApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE")
+				.allowedOrigins("*")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 
 	@Bean
