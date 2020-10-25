5460ac87fcd59e36e429f865f3640f129bcfa5e1
Dani Villegas <dani.villegascarp@gmail.com>
2020-10-25 20:16:06 -0300

    Fix register

--- src/main/java/ar/edu/unq/cookitbackend/service/IAuthService.java
+++ src/main/java/ar/edu/unq/cookitbackend/service/IAuthService.java
@@ -3,10 +3,11 @@ package ar.edu.unq.cookitbackend.service;
 import ar.edu.unq.cookitbackend.dto.request.LoginRequestDto;
 import ar.edu.unq.cookitbackend.dto.request.UserRequestDto;
 import ar.edu.unq.cookitbackend.dto.response.JwtResponse;
+import ar.edu.unq.cookitbackend.exception.EmailExistException;
 import ar.edu.unq.cookitbackend.exception.NotFoundException;
 
 public interface IAuthService {
 
-    void register(UserRequestDto request) throws NotFoundException;
+    void register(UserRequestDto request) throws NotFoundException, EmailExistException;
     JwtResponse loginSocial(LoginRequestDto request);
 }
