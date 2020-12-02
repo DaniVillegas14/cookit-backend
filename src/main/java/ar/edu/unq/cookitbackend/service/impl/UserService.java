4ba3ea3c5bce0ee76f2143355c91112d91ae8156
Daniel Villegas <dani.villegascarp@gmail.com>
2020-12-02 20:12:12 -0300

    Fix change password

--- src/main/java/ar/edu/unq/cookitbackend/service/impl/UserService.java
+++ src/main/java/ar/edu/unq/cookitbackend/service/impl/UserService.java
@@ -38,6 +38,8 @@ public class UserService implements IUserService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
